66e44ed6f304d6b128c39f9ecc3efc6aff2cba77
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-30 01:40:59 +0900

    graphql下準備

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: "https://api-ap-northeast-1.hygraph.com/v2/clc9arzpp1lzs01tcafic35lp/master",
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 )
