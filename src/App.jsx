d2c4f328fca87e68e5a90a2b4825b1e7d31aee4a
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-30 01:54:05 +0900

    graphQLのデータをReactで取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import './App.css'
+import { gql, useQuery } from '@apollo/client'
+
+const DOGS = gql`
+  query Dogs {
+    dogs {
+      id
+      name
+      description
+      thumbnail {
+        url
+      }
+    }
+  }
+`
 
 function App() {
+  // console.log(DOGS)
+  console.log(useQuery(DOGS))
+
+  const { data, loading, error} = useQuery(DOGS)
+
+  if(loading) return "ロード中..."
+  if(error) return `エラー！${error}`
 
   return (
-    <div className="App"></div>
+    <>
+      <h1>GraphQLとReact</h1>
+      <div className='dogsContainer'>
+        {data.dogs.map((dog) => (
+          <div key={dog.id}>
+            <div className='dogCard'>
+              <img src={dog.thumbnail.url} alt="" />
+              <p>{dog.name}</p>
+              <p>{dog.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
